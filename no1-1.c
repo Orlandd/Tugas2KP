deb73c771cadccf867735ad75caf87905bbf0983
Orlandd <109672644+Orlandd@users.noreply.github.com>
2022-12-10 21:53:24 +0700

    Soal A no 1

---
+++ no1-1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+
+    char merk[100];
+    char space[] = " ";
+    char serial[100];
+    int lenght;
+
+    printf("Merk handphone \t\t: ");
+    gets(merk);
+    printf("Serial handphone \t: ");
+    gets(serial);
+    
+    printf("\n======================================\n");
+    printf("\nMerk handphone \t\t: %s\n", merk);
+    printf("Serial handphone \t: %s\n", serial);
+
+    // Menggabungkan string
+    strcat(merk, space);
+    strcat(merk, serial);
+
+    lenght = strlen(merk);
+    printf("\n======================================\n");
+    printf("\nHandphone \t: %s\n", merk);
+    printf("panjang \t: %d karakter", lenght);
+
+}
